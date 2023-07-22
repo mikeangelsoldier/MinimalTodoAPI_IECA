83583b1327ff2f030f259f3c41d36f9962157c89
mikeangelsoldier <mikeangelsoldier1@gmail.com>
2023-07-21 22:51:08 -0600

    First commit

---
+++ TodoDb.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
